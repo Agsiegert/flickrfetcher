f1ac7eb98d496d90e43730398867b1ea1ed0a801
Antony Siegert <antony.siegert@infopark.de>
2017-04-27 10:30:39 +0200

    berlin flickr fetcher tdd

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-FlickrFetcher.fetchPhotos('a0cfc52727310dea5b7f672ac0f0e8db')
+FlickrFetcher.fetchPhotos('xxxa0cfc52727310dea5b7f672ac0f0e8db')
     .then(PhotoLister.photoListToHTML)
     .then(function(photosHTML) {
         PhotoLister.addPhotosToElement($, '#mydiv', photosHTML)
